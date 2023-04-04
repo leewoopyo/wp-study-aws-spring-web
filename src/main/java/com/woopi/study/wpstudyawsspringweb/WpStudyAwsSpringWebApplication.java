a65d46bd6d1b56c266b97e299d2dcc9347452808
Lstamp <lozenkrantz@gmail.com>
2023-04-05 02:34:54 +0900

    main class 수정

--- src/main/java/com/woopi/study/wpstudyawsspringweb/WpStudyAwsSpringWebApplication.java
+++ src/main/java/com/woopi/study/wpstudyawsspringweb/WpStudyAwsSpringWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WpSdutyAwsSpringWebApplication {
+public class WpStudyAwsSpringWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WpSdutyAwsSpringWebApplication.class, args);
+		SpringApplication.run(WpStudyAwsSpringWebApplication.class, args);
 	}
 
 }
--- src/test/java/com/woopi/study/wpstudyawsspringweb/WpStudyAwsSpringWebApplicationTests.java
+++ src/test/java/com/woopi/study/wpstudyawsspringweb/WpStudyAwsSpringWebApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WpSdutyAwsSpringWebApplicationTests {
+class WpStudyAwsSpringWebApplicationTests {
 
 	@Test
 	void contextLoads() {
