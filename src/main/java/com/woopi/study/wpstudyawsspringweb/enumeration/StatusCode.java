24da464b6ece42671f6cdc09847f83141b139cf6
Lstamp <lozenkrantz@gmail.com>
2023-04-12 02:18:50 +0900

    StatusCode 코드 추가

--- src/main/java/com/woopi/study/wpstudyawsspringweb/enumeration/StatusCode.java
+++ src/main/java/com/woopi/study/wpstudyawsspringweb/enumeration/StatusCode.java
@@ -1,7 +1,16 @@
 package com.woopi.study.wpstudyawsspringweb.enumeration;
 
 public enum StatusCode {
+    /*성공*/
     SUCCESS("0000", null),
+
+    /*공통 메시지*/
+    PARAMETER_NOT_FOUND("1001", "필수 파라메터가 없습니다."),
+
+    /*유저 관련 메시지*/
+    USER_INFO_NOT_FOUND("2001", "유저 정보를 찾을 수 없습니다."),
+
+    /*알수 없는 오류*/
     UNKNOWN_EXCEPTION("9999", "알수없는 오류가 발생했습니다. 관리자에게 문의하세요.");
 
     String code;
