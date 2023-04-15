04d97613cbe6a30042f5e500e1df56ade997509e
Lstamp <lozenkrantz@gmail.com>
2023-04-16 04:41:43 +0900

    1.mustache 의존성 추가 2.readme.md 수정 3.index.mustache 추가 4.test작성 5.header, footer 추가

--- src/main/java/com/woopi/study/wpstudyawsspringweb/web/controller/WebController.java
+++ src/main/java/com/woopi/study/wpstudyawsspringweb/web/controller/WebController.java
@@ -1,13 +1,18 @@
-package com.woopi.study.wpstudyawsspringweb.pages.controller;
+package com.woopi.study.wpstudyawsspringweb.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class pageController {
+public class WebController {
     @GetMapping("/swagger-ui")
     public String swagger() {
         return "redirect:/swagger-ui/index.html";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 
 }
